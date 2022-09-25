5663ce00ef53b98ed543e3ee795c0fbd25a9daaa
Totoro <trungle30902@gmail.com>
2022-09-25 23:36:18 +0700

    + [new] - fastapi crud

---
+++ stack-playground/220925_fastapi-crud/backend/main.py
@@ -0,0 +1,20 @@
+from lib2to3.refactor import MultiprocessingUnsupported
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from routes import router as book_router
+
+config = dotenv_values(".env")
+
+app = FastAPI()
+
+@app.on_event("startup")
+def startup_db_client():
+  app.mongodb_client = MongoClient('localhost', 27017)
+  app.database = app.mongodb_client['crud']
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+  app.mongodb_client.close()
+
+app.include_router(book_router, tags=["boogks"], prefix="/book")
